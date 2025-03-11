6526da8c076e013d2a4f71b33c9dac3beeabf20a
Abu Sakib <mabusakib@gmail.com>
2025-03-11 23:30:48 +0600

    Implement safe parsing when `body` is not base64-encoded (#4)
    * Initial implementation of base64 check
    
    * Revise logic and clean up
    
    * Revise logic and update tests

--- utils.go
+++ utils.go
@@ -1,15 +1,19 @@
 package moesifawslambda
 
 import (
-	"net/url"
-	"log"
-	"time"
+	b64 "encoding/base64"
 	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+	"regexp"
+	"time"
+
 	"github.com/aws/aws-lambda-go/events"
 	models "github.com/moesif/moesifapi-go/models"
-	b64 "encoding/base64"
-	"net/http"
 )
+
+const Base64 string = "^[A-Za-z0-9+/]+={0,2}$"
 
 func prepareRequestURI(request events.APIGatewayProxyRequest) string {
 	var uri string
@@ -57,28 +61,34 @@ func prepareRequestURI(request events.APIGatewayProxyRequest) string {
 	return uri
 }
 
+func isBase64String(str string) bool {
+	b64Regex, err := regexp.Compile(Base64)
+	if err != nil {
+		return false
+	}
+	return b64Regex.MatchString(str)
+}
+
 func processBody(body string) (interface{}, string) {
 	var parsedBody interface{}
 	var transferEncoding string
 
 	parsedBody = nil
 	transferEncoding = "json"
-	if logBody {
-		if jsonMarshalErr := json.Unmarshal([]byte(body), &parsedBody); jsonMarshalErr != nil {
-			if debug {
-				log.Printf("About to parse request body as base64 ")
-			}
-			parsedBody = b64.StdEncoding.EncodeToString([]byte(body))
-			transferEncoding = "base64"
-			if debug {
-				log.Printf("Parsed request body as base64 - %s", parsedBody)
-			}
+	if jsonMarshalErr := json.Unmarshal([]byte(body), &parsedBody); jsonMarshalErr != nil {
+		if debug {
+			log.Printf("About to parse request body as base64 ")
+		}
+		parsedBody = b64.StdEncoding.EncodeToString([]byte(body))
+		transferEncoding = "base64"
+		if debug {
+			log.Printf("Parsed request body as base64 - %s", parsedBody)
 		}
 	}
 	return parsedBody, transferEncoding
 }
 
-func processHeaders(headers map[string] string) map[string] string {
+func processHeaders(headers map[string]string) map[string]string {
 	// Check if the headers are empty
 	if len(headers) == 0 {
 		var emptyHeaders = map[string]string{}
@@ -99,33 +109,54 @@ func defaultSourceIp(request events.APIGatewayProxyRequest) *string {
 func prepareEvent(request events.APIGatewayProxyRequest, response events.APIGatewayProxyResponse, apiVersion *string, userId *string, companyId string, sessionToken string, metadata map[string]interface{}) models.EventModel {
 
 	reqTime := time.Now().UTC()
-	transformReqBody, transferEncoding := processBody(request.Body)
+	var transformReqBody interface{} = nil
+	var transferEncoding string = "json"
+
+	if logBody && len(request.Body) != 0 {
+		if request.IsBase64Encoded && isBase64String(request.Body) {
+			transformReqBody = request.Body
+			transferEncoding = "base64"
+		} else {
+			transformReqBody, transferEncoding = processBody(request.Body)
+		}
+	}
 
 	var transformReqHeaders = make(map[string][]string)
-	for  key, value := range request.Headers {
+	for key, value := range request.Headers {
 		transformReqHeaders[key] = []string{value}
 	}
 
 	eventRequestModel := models.EventRequestModel{
-		Time:       &reqTime,
-		Uri:        prepareRequestURI(request),
-		Verb:       request.HTTPMethod,
-		ApiVersion: apiVersion,
-		IpAddress: getClientIp(transformReqHeaders, defaultSourceIp(request)),
-		Headers: processHeaders(request.Headers),
-		Body: &transformReqBody,
+		Time:             &reqTime,
+		Uri:              prepareRequestURI(request),
+		Verb:             request.HTTPMethod,
+		ApiVersion:       apiVersion,
+		IpAddress:        getClientIp(transformReqHeaders, defaultSourceIp(request)),
+		Headers:          processHeaders(request.Headers),
+		Body:             &transformReqBody,
 		TransferEncoding: &transferEncoding,
 	}
 
 	rspTime := time.Now().UTC()
-	transformRespBody, transferEncoding := processBody(response.Body)
+
+	var transformRespBody interface{}
+	transferEncoding = "json"
+
+	if logBody && len(response.Body) != 0 {
+		if response.IsBase64Encoded && isBase64String(response.Body) {
+			transformRespBody = response.Body
+			transferEncoding = "base64"
+		} else {
+			transformRespBody, transferEncoding = processBody(response.Body)
+		}
+	}
 
 	eventResponseModel := models.EventResponseModel{
-		Time:      &rspTime,
-		Status:    response.StatusCode,
-		IpAddress: nil,
-		Headers: processHeaders(response.Headers),
-		Body: &transformRespBody,
+		Time:             &rspTime,
+		Status:           response.StatusCode,
+		IpAddress:        nil,
+		Headers:          processHeaders(response.Headers),
+		Body:             &transformRespBody,
 		TransferEncoding: &transferEncoding,
 	}
 
@@ -139,7 +170,7 @@ func prepareEvent(request events.APIGatewayProxyRequest, response events.APIGate
 		Tags:         nil,
 		UserId:       userId,
 		CompanyId:    &companyId,
-		Metadata: 	  &metadata,
+		Metadata:     &metadata,
 		Direction:    &direction,
 		Weight:       &weight,
 	}
@@ -148,7 +179,7 @@ func prepareEvent(request events.APIGatewayProxyRequest, response events.APIGate
 
 // Send Outgoing Event to Moesif
 func sendMoesifOutgoingAsync(request *http.Request, reqTime time.Time, apiVersion *string, reqBody interface{}, reqEncoding *string,
-	rspTime time.Time, respStatus int, respHeader http.Header, respBody interface{}, respEncoding *string, userId *string, 
+	rspTime time.Time, respStatus int, respHeader http.Header, respBody interface{}, respEncoding *string, userId *string,
 	companyId *string, sessionToken *string, metadata map[string]interface{}, direction *string, weight *int) {
 
 	// Get Client Ip
@@ -156,47 +187,47 @@ func sendMoesifOutgoingAsync(request *http.Request, reqTime time.Time, apiVersio
 
 	// Prepare request model
 	event_request := models.EventRequestModel{
-	Time:       &reqTime,
-	Uri:        request.URL.Scheme + "://" + request.Host + request.URL.Path,
-	Verb:       request.Method,
-	ApiVersion: apiVersion,
-	IpAddress:  ip,
-	Headers:    request.Header,
-	Body: 		&reqBody,
-	TransferEncoding: reqEncoding,
+		Time:             &reqTime,
+		Uri:              request.URL.Scheme + "://" + request.Host + request.URL.Path,
+		Verb:             request.Method,
+		ApiVersion:       apiVersion,
+		IpAddress:        ip,
+		Headers:          request.Header,
+		Body:             &reqBody,
+		TransferEncoding: reqEncoding,
 	}
 
 	// Prepare response model
 	event_response := models.EventResponseModel{
-	Time:      &rspTime,
-	Status:    respStatus,
-	IpAddress: nil,
-	Headers:   respHeader,
-	Body: 	   respBody,
-	TransferEncoding: respEncoding,
+		Time:             &rspTime,
+		Status:           respStatus,
+		IpAddress:        nil,
+		Headers:          respHeader,
+		Body:             respBody,
+		TransferEncoding: respEncoding,
 	}
 
 	// Prepare the event model
 	event := models.EventModel{
-	Request:      event_request,
-	Response:     event_response,
-	SessionToken: sessionToken,
-	Tags:         nil,
-	UserId:       userId,
-	CompanyId:    companyId,
-	Metadata: 	  metadata,
-	Direction:    direction,
-	Weight:       weight,
+		Request:      event_request,
+		Response:     event_response,
+		SessionToken: sessionToken,
+		Tags:         nil,
+		UserId:       userId,
+		CompanyId:    companyId,
+		Metadata:     metadata,
+		Direction:    direction,
+		Weight:       weight,
 	}
 
 	// Send event to moesif
 	_, err := apiClient.CreateEvent(&event)
-	
+
 	// Log the message
 	if err != nil {
 		log.Fatalf("Error while sending event to Moesif: %s.\n", err.Error())
 	}
-	
+
 	if debug {
 		log.Printf("Successfully sent outgoing event to Moesif")
 	}
